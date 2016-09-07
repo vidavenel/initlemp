a5248b7cda85223b2a45aad925423fb4492441c0
vidavenel <vincent@aiglecom.com>
2016-09-07 16:05:07 +0200

    fix tar wp

--- files/wp/init.sh
+++ files/wp/init.sh
@@ -3,13 +3,14 @@
 mkdir /data/rootdir/php
 mkdir /data/rootdir/nginx
 mkdir /data/rootdir/nginx/conf.d
+mkdir /data/rootdir/public_html
 
 cp /data/wp/php.ini /data/rootdir/php/php.ini
 cp /data/wp/wp.conf /data/rootdir/nginx/conf.d/wp.conf
 
 cd /data/rootdir/public_html
-wget https://wordpress.org/latest.tar.gz --no-check-certificate
-tar -xzvf latest.tar.gz
-rm latest.tar.gz
-mv wordpress/* .
-rm wordpress -R
+wget https://wordpress.org/latest.tar.gz --no-check-certificate \
+	tar -xzvf latest.tar.gz \
+	rm latest.tar. \
+	mv wordpress/* . \
+	rm wordpress -R
